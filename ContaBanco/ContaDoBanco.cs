f22e732ea1d18d42d68d4013ebc49ef6d45be9fe
Raisa Almeida <almeida.alves.raisa@gmail.com>
2023-01-12 23:28:11 -0300

    first commit

---
+++ ContaBanco/ContaDoBanco.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBanco { 
+
+internal class ContaDoBanco
+{
+    public int NumConta { get; set; }
+
+    protected string Tipo { get; set; }
+
+    private string Dono { get; set; }
+
+    private double Saldo { get; set; }
+
+    private bool Status { get; set; }
+
+
+
+    public ContaDoBanco(int saldo)
+    {
+        Saldo = 0;
+        setStatus(false);
+    }
+
+    public void mostrarDetalhes()
+        {
+            Console.WriteLine("//////////////////////////////////////////////");
+            Console.WriteLine("Tipo: " + Tipo);
+            Console.WriteLine("Nome: " + Dono);
+            Console.WriteLine("Numero da Conta: " + NumConta);
+            Console.WriteLine("Saldo: " + Saldo);
+            Console.WriteLine("Status: " + Status);
+
+
+            Console.WriteLine("//////////////////////////////////////////////");
+
+       }
+        public void setStatus(bool status)
+    {
+        Status = status;
+    }
+
+    public void setNumConta(int numConta)
+    {
+        NumConta = numConta;
+    }
+
+    public void setTipo(string tipo)
+    {
+        Tipo = tipo;
+    }
+     public void setDono(string dono)
+     {
+           Dono = dono;
+     }
+
+     public int getNumConta()
+        {
+          return NumConta;
+     }
+        public void  abrirConta(string type)
+    {
+        Tipo = type;
+        setStatus(true);
+
+        if (type == "CC")
+        {
+            Saldo = 50;
+        }
+        else if (type == "CP")
+        {
+            Saldo = 150;
+        }
+    }
+
+    public void fecharConta()
+    {
+        if (Saldo > 0)
+        {
+            Console.WriteLine("Conta com dinheiro");
+        }
+        else if (Saldo < 0)
+        {
+            Console.WriteLine("Conta em débito");
+        }
+        else
+        {
+            setStatus(false);
+        }
+
+    }
+
+    public void depositar(double dep)
+    {
+            Console.WriteLine(Status);
+            if (Status == true)
+            {
+               Saldo = Saldo + dep;
+            }
+            else
+            {
+            Console.WriteLine("Impossível Depositar");
+          
+            }
+        }
+
+    public void sacar(double sac)
+    {
+        if (Status == true)
+        {
+            if (Saldo >= sac)
+            {
+                Saldo = Saldo - sac;
+            }
+            else
+            {
+                Console.WriteLine("Saldo Insuficiente");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Impossível Sacar");
+        }
+
+    }
+
+    public void pagarMensal()
+    {
+        double pagMen = 0;
+
+        if (Tipo == "CC")
+        {
+            pagMen = 12;
+        }
+        else if (Tipo == "CP")
+        {
+            pagMen = 20;
+        }
+
+        if (Status == true)
+        {
+                if (Saldo >= pagMen)
+                {
+                    Saldo = Saldo - pagMen;
+                }
+                else
+                {
+                    Console.WriteLine("Saldo Insuficiente");
+
+                }
+
+            }
+        else
+        {
+            Console.WriteLine("Impossível pagar");
+        }
+    }
+
+        //internal bool abrirConta()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
+
+
---
+++ ContaBanco/Program.cs
@@ -0,0 +1,111 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ContaBanco;
+using System.Xml.Linq;
+
+var listaDeContas = new List<ContaDoBanco>();
+
+string opt = "";
+
+
+do
+{
+    Console.WriteLine("Escolha um das opções");
+    Console.WriteLine("0:Finalizar Operação");
+    Console.WriteLine("1: Abrir Conta");
+    Console.WriteLine("2:Mostrar  Contas");
+    Console.WriteLine("3: Fechar Conta ");
+    Console.WriteLine("4: Depositar");
+    Console.WriteLine("5: Sacar");
+    Console.WriteLine("6: Pagar Mensalidade");
+
+    opt = Console.ReadLine();
+
+    
+    if (opt == "1")
+    {
+        var conta = new ContaDoBanco(0);
+        Console.WriteLine("Digite CC para Conta Corrente e CP para Conta Poupança");
+        conta.abrirConta(Console.ReadLine());
+        Console.WriteLine("Digite o seu nome: ");
+        conta.setDono(Console.ReadLine());
+        Console.WriteLine("Digite o numero da conta");
+        conta.setNumConta(Convert.ToInt32(Console.ReadLine()));
+
+        
+        listaDeContas.Add(conta);
+       
+    }
+    if (opt == "2")
+    {
+        {
+            foreach (ContaDoBanco conta in listaDeContas)
+            {
+                conta.mostrarDetalhes();
+            }
+        }
+    }
+
+    if (opt == "3")
+    {
+        Console.WriteLine("Digite o número da conta: ");
+        var numeConta = (Convert.ToInt32(Console.ReadLine()));
+        ContaDoBanco conta = listaDeContas.Find(x => x.getNumConta() == numeConta);
+        conta.fecharConta();
+    }
+
+    if (opt == "4")
+    {
+        Console.WriteLine("Digite o número da conta: ");
+        var numeConta = (Convert.ToInt32(Console.ReadLine()));
+        ContaDoBanco conta = listaDeContas.Find(x => x.getNumConta() == numeConta);
+        Console.WriteLine(" Digite o valor que você deseja depositar ");
+        var deposito = Convert.ToDouble(Console.ReadLine());
+        conta.depositar(deposito);
+
+
+
+    }
+
+    if (opt == "5")
+    {
+        Console.WriteLine("Digite o número da conta: ");
+        var numeConta = (Convert.ToInt32(Console.ReadLine()));
+        ContaDoBanco conta = listaDeContas.Find(x => x.getNumConta() == numeConta);
+        var saque = Convert.ToDouble(Console.ReadLine());
+        conta.sacar(saque);
+
+
+
+
+    }
+
+    if (opt == "6")
+    {
+        Console.WriteLine("Digite o número da conta: ");
+        var numeConta = (Convert.ToInt32(Console.ReadLine()));
+        ContaDoBanco conta = listaDeContas.Find(x => x.getNumConta() == numeConta);
+        conta.pagarMensal();
+
+    }
+
+    //if (opt == "4")
+    //{
+    //    Console.WriteLine("Digite o nome da caneta que você deseja usar ");
+    //    var name = Console.ReadLine();
+    //    Caneta caneta = listaDeCanetas.Find(x => x.getNome() == name);
+    //    Console.WriteLine("Digite a palavra que deseja escrever");
+    //    var palavra = Console.ReadLine();
+    //    caneta.Rabiscar(palavra);
+
+
+    //}
+
+
+
+
+} while (opt != "0") ; 
+
+    Console.WriteLine("Programa finalizado");
+
+
